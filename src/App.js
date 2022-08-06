0d43952b2ec47c06e67a8ebf914d34616e6cfb2d
nealgandhi <gandhi.neal2002@gmail.com>
2022-08-06 08:59:06 -0500

    Bg changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './input.css'
-import Body from './components/Body.js';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './components/Home';
-
+import "./input.css";
+import Body from "./components/Body.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="bg-black">
+    <div className="bg-black h-screen">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/search" element={<Body />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Body />} />
         </Routes>
       </BrowserRouter>
     </div>
